19c91d4500cd6798ff6b6dfd5cc683451a70c632
buffard <samuelwebber19@gmail.com>
2018-11-16 14:37:00 -0600

    working finished practice

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
-import EmployeeList from "../employee/EmployeeList"  // Import EmployeeList component
-
+import EmployeeList from "./employee/EmployeeList"  // Import EmployeeList component
+import LocationList from "./LocationList/LocationList"
 
 export default class Kennel extends Component {
     render() {
         return (
             <div>
                 <h3>Student Kennels</h3>
-                <h4>Nashville North Location</h4>
-                <h5>500 Puppy Way</h5>
+                <LocationList />
                 <EmployeeList />
             </div>
-        );
+        )
     }
 }
